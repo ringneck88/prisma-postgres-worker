f04e70f1624da93e101baca52d3ebf9426460af8
Kelly Sharp <kelly@letsgomint.com>
2025-06-25 11:10:53 -0700

    updated

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import { PrismaClient } from "@prisma/client/edge";
 import { withAccelerate } from "@prisma/extension-accelerate";
 
-
+const prisma = new PrismaClient().$extends(withAccelerate())
 
 
 export default {

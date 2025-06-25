35daadf001209d97a9b5d6f1f2cea4bdd4e9edc6
Kelly Sharp <kelly@letsgomint.com>
2025-06-25 11:21:27 -0700

    updaate

--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
 import { PrismaClient } from "@prisma/client/edge";
 import { withAccelerate } from "@prisma/extension-accelerate";
 
-const prisma = new PrismaClient().$extends(withAccelerate())
-
-
 export default {
 	async fetch(request, env, ctx) {
 		if (request.method !== "POST") {

c5b5dd4e9ce6c6e261a52f732b01ea0b7b2128ed
kelly-brightroot <ksharp@brightroot.com>
2025-07-06 14:40:35 -0700

    sm

--- src/index.js
+++ src/index.js
@@ -64,7 +64,7 @@ async function saveToDatabase(data, prisma) {
 
 		return result; // Ensure this is a valid JSON object
 	} catch (error) {
-		console.error("Database ----- error:"+"url = "+prisma.$datasourceUrl+ "  " + error);
+		console.error("Database ----- error:"+"url = "+prisma.datasourceUrl+ "  " + error);
 		throw new Error("Failed to save data.");
 	}
 }
